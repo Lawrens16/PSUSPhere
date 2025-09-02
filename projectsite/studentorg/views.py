d9196668badd9186fd202ac41dfdcbaac22114a3
Netoerare <lawsmagnet6@gmail.com>
2025-09-02 22:29:25 +0800

    completed organization crud

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -3,7 +3,7 @@ from django.views.generic.list import ListView
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 class HomePageView(ListView):
     model = Organization
@@ -26,4 +26,9 @@ class OrganizationUpdateView(UpdateView):
     model = Organization
     form_class = OrganizationForm
     template_name = 'org_form.html'
+    success_url = reverse_lazy('organization-list') 
+
+class OrganizationDeleteView(DeleteView):
+    model = Organization
+    template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list') 
